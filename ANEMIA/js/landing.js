7960980c9cc180af497b78421fac60107bbae3bb
RocioCVidal <147789961+RocioCVidal@users.noreply.github.com>
2023-12-01 08:46:38 -0500

    Add files via upload

---
+++ ANEMIA/js/landing.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function() {
+    fetch('/js/Datos_Anemia_2021.json')
+        .then(response => response.json())
+        .then(data => {
+            const tabla = document.getElementById('datos-Anemia');
+            tabla.innerHTML = '<tr><th>Departamento</th><th>Distrito</th><th>Total de niños</th><th>Niños con Anemia</th><th>Porcentaje de Niños con Anemia</th><th>Porcentaje de atención médica</th></tr>';
+            data.forEach(registro => {
+                tabla.innerHTML += `<tr>
+                    <td>${registro['Departamento']}</td>
+                    <td>${registro['Distrito']}</td>
+                    <td>${registro['Total de niños']}</td>
+                    <td>${registro['Niños con Anemia']}</td>
+                    <td>${registro['Porcentaje de Niños con Anemia']}</td>
+                    <td>${registro['Porcentaje de atención médica']}</td>
+                </tr>`;
+            });
+        });
+});
